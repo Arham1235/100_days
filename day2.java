eec1974e492ceff247809ee8d9cc74f74dc3d688
Arham <147286846+Arham1235@users.noreply.github.com>
2023-10-10 22:46:26 +0800

    Add files via upload
    saya hari ini membuat variabel biodata diri

---
+++ day2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arham
+ */
+public class day2 {
+    public static void main(String[] args) {
+        System.out.println("---------BIODATA DIRI--------");
+        System.out.println("Nama    : arham");
+        System.out.println("umur    : 19 tahun");
+        System.out.println("alamat  : polewali");
+        System.out.println("hobby   : rebahan");
+    }
+    
+}
