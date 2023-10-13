9b4a9f24f8fd162d662d8f3a24b6a61ed85d5df2
Arham <147286846+Arham1235@users.noreply.github.com>
2023-10-13 23:09:52 +0800

    Add files via upload
    hari ini saya masih mempelajari percabangan

---
+++ percabanganDays5.java
@@ -0,0 +1,26 @@
+
+ import java.util.Scanner;
+public class percabanganDays5 {
+
+    public static void main(String[] args) {
+
+        // membuat variabel belanja dan scanner
+        int belanja = 1;
+        Scanner scan = new Scanner(System.in);
+
+        // mengambil input
+        System.out.print("Total Belanjaan: Rp. ");
+        belanja = scan.nextInt();
+
+        // cek apakah dia belanja di atas 100000
+        if ( belanja > 100000 ) {
+            System.out.println("Selamat, anda mendapatkan hadiah!");
+        }
+
+        System.out.println("Terima kasih...");
+
+    }
+
+}
+    
+
