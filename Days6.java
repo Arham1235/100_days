ceb544d471ea452b18562f6a23394427c7bbdafe
Arham <147286846+Arham1235@users.noreply.github.com>
2023-10-14 20:27:50 +0800

    Add files via upload
    hari ini saya membuat program data diri

---
+++ Days6.java
@@ -0,0 +1,19 @@
+
+public class Days6 {
+    public static void main(String[] args) {
+        String nama, alamat;
+        int usia;
+        double tinggi;
+        
+        nama = "arham";
+        alamat = "majene";
+        usia = 19;
+        tinggi = 159.7;
+        
+        System.out.println("Nama :"+nama);
+        System.out.println("Alamat :" + alamat);
+        System.out.println("Usia :" + usia + "tahun");
+        System.out.println("tinggi :" + tinggi+"cm");
+                
+    }
+}
