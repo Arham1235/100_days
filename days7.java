65987caac9eee9106db53591d875fa83a433dc34
Arham <147286846+Arham1235@users.noreply.github.com>
2023-10-15 21:21:40 +0800

    Add files via upload
    hari ini saya membuat biodata diri

---
+++ days7.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arham
+ */
+public class days7 {
+    public static void main(String[] args) {
+        String nama, agama , alamat;
+     
+        nama = "arham";
+        agama = "islam";
+        alamat = "polewali";
+        
+        System.out.println("=====================biodata=================");
+        System.out.println("Nama :" +nama);
+        System.out.println("Agama : "+agama);
+        System.out.println("Alamat :"+alamat);
+        System.out.println("=============================================");
+        
+        
+    }
+    
+}
