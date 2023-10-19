02146136d6abb91760894be64de3f708e03ce32c
Arham <147286846+Arham1235@users.noreply.github.com>
2023-10-19 21:27:04 +0800

    Add files via upload
    hari ini saya membuat perulangan dengan yang mencetak angka 1 sampai 100 menggunakan do/while

---
+++ Days11Dowhile.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arham
+ */
+public class Days11Dowhile {
+     public static void main(String[] args) {
+        int Nomor = 1;
+            do {
+                System.out.println("Nomor:" + Nomor);
+                Nomor++;
+                }
+            while (Nomor <= 100);
+    }
+}
+
