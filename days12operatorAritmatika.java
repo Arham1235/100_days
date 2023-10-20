ac8dbfffef968dfda1af36ba0007e98613834b2c
Arham <147286846+Arham1235@users.noreply.github.com>
2023-10-20 20:06:13 +0800

    Add files via upload
    hari ini saya menggunakan operator aritmatikan dengan kombinasi scanner

---
+++ days12operatorAritmatika.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+
+public class days12operatorAritmatika {
+    public static void main(String[] args) {
+        
+    Scanner ar = new Scanner(System.in);
+        System.out.print("umur: ");
+        int umr = ar.nextInt();
+        int hari = 360 *umr;
+        System.out.println("lama hidup :" +hari + " hari");
+        
+    } 
+       
+    
+}
