5adc80f09124a2911cc2caf037769d1104e0767f
Arham <147286846+Arham1235@users.noreply.github.com>
2023-10-21 21:24:42 +0800

    Add files via upload
    hari ini saya coding menggunakan array

---
+++ Days13array.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arham
+ */
+public class Days13array {
+    public static void main(String[] args) {
+        
+        int[] angka = new int[5]; 
+        angka[0] = 10;
+        angka[1] = 20;
+        angka[2] = 30;
+
+
+        int nilai = angka[2];
+        System.out.println("Nilai ketiga Dalam Array Adalah:"+nilai);
+    }
+    
+}
