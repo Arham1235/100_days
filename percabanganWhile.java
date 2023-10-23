b940b13119f1033c36a88db2b9d8b98a901a0082
Arham <147286846+Arham1235@users.noreply.github.com>
2023-10-23 21:28:08 +0800

    Add files via upload
    percabangan while

---
+++ percabanganWhile.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arham
+ */
+public class percabanganWhile {
+     public static void main(String[] args) {
+        int a=1;
+         System.out.println("=========== Daftar juara lomba lari =========");
+        while(a<=5){
+           
+            System.out.println("juara "+a);
+            a++;
+        }
+        System.out.println("Selamat kepada para pemenan");
+    }
+}
