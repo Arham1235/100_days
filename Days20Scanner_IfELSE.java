1aa2898651d46ed456cef9b0c76b502e3bdb8e07
Arham <147286846+Arham1235@users.noreply.github.com>
2023-10-28 19:54:10 +0800

    Add files via upload
    hari ini saya membuat program mengunakan scanner dengan megombinasikan if else

---
+++ Days20Scanner_IfELSE.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arham
+ */
+public class Days20Scanner_IfELSE {
+    public static void main(String[] args) {
+        Scanner hm = new Scanner(System.in);
+        int angka;
+        System.out.print("Masukkan angka :");
+        angka = hm.nextInt();
+        
+        if(angka>=1){
+            System.out.println(" Bilangan Positif");
+            }else{
+                System.out.println("Bilangan Negatif");
+            }
+        
+            
+        }
+    }
+    
+
