611a8e9c995b30598fce2cfd18db2806183c1ee0
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-02 20:07:24 +0800

    Add files via upload
    percabangan Switch/case

---
+++ Days25_Switch_case.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author arham
+ */
+public class Days25_Switch_case {
+     public static void main(String[] args) {
+
+        
+        String lampu;
+        Scanner scan = new Scanner(System.in);
+
+       
+        System.out.print("Warna lampu : ");
+        lampu = scan.nextLine();
+
+        switch(lampu){
+            case "merah":
+                System.out.println("Lampu merah, berhenti!");
+                break;
+            case "kuning":
+                System.out.println("Lampu kuning, harap hati-hati!");
+                break;
+            case "hijau":
+                System.out.println("Lampu hijau, silahkan jalan!");
+                break;
+            default:
+                System.out.println("Warna lampu salah!");
+        }
+    }
+    
+}
