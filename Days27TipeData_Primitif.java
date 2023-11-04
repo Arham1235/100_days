92cd9d29a544c7eadfcb5e749ba5885cc55567f4
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-04 18:21:14 +0800

    Add files via upload
    cara menampilkan nilai maksimal, nilai minimal, tipe data dan besar Byte

---
+++ Days27TipeData_Primitif.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days27TipeData_Primitif {
+    public static void main(String[] args) {
+       
+        System.out.println("====== BYTE =====");
+        System.out.println("Nilai minimal = " + Byte.MIN_VALUE);
+        System.out.println("Nilai maxsimun = "+Byte.MAX_VALUE);
+        System.out.println("Besar Byte = " + Byte.SIZE);
+        System.out.println("Tipe Data  = " + Byte.TYPE);
+        
+        System.out.println("===================");  
+        System.out.println(" ");
+        
+        System.out.println("======== Short ==========");
+        System.out.println("Nilai minimal = " + Short.MIN_VALUE );
+        System.out.println("Nilai maxsimun = "+ Short.MAX_VALUE);
+        System.out.println("Besar Byte = " + Short.SIZE);
+        System.out.println("Tipe Data  = " + Short.TYPE);
+        System.out.println("==========================");
+        System.out.println(" ");
+        
+         System.out.println("======== Integer ==========");
+        System.out.println("Nilai minimal = " + Integer.MIN_VALUE);
+        System.out.println("Nilai maxsimun = "+ Integer.MIN_VALUE);
+        System.out.println("Besar Byte = " + Integer.SIZE);
+        System.out.println("Tipe Data  = " + Integer.TYPE );
+        System.out.println("==========================");
+        System.out.println(" ");
+        
+    }
+    
+}
