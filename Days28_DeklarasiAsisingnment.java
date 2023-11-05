9b8e51dc4a81f545d3ce96a9c0edb94133424e8b
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-05 21:59:48 +0800

    Add files via upload
    hari ini membuat deklarasi dan assingment

---
+++ Days28_DeklarasiAsisingnment.java
@@ -0,0 +1,16 @@
+
+package ISC_Project;
+
+
+public class Days28_DeklarasiAsisingnment {
+    public static void main(String[] args) {
+         int a = 20;
+         int b;
+         b = 10;
+         
+        System.out.println("Nilai a = " +a);
+        System.out.println("Nilai b = " + b);
+        System.out.println("A + B = " +(a+b));
+    }
+    
+}
