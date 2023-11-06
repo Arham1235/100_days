aa3451183622b89a907ca18630dcce32190cec37
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-06 23:22:10 +0800

    Add files via upload
    hari ini saya membuat biodata sederhana saya menggunakan variabel dan tipe data

---
+++ Days29VariableDanTipeData.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days29VariableDanTipeData {
+    public static void main(String[] args) {
+        int umur;
+        double tinggi;
+        String nama;
+        
+        nama = "ARHAM";
+        umur = 20;
+        tinggi = 160.9;
+        
+        System.out.println("Nama :" + nama);
+        System.out.println("Umur : "+umur +" Tahun");
+        System.out.println("Tinggi badan : "+tinggi +" Cm");
+        
+    }
+    
+}
