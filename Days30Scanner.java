dfb009434f0d2ea0df05e898766d39b255530c0f
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-07 22:39:51 +0800

    Add files via upload
    mengambil inputan menggunaka Scanner

---
+++ Days30Scanner.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arham
+ */
+public class Days30Scanner {
+    public static void main(String[] args) {
+        String nama, alamat;
+        int umur;
+
+        Scanner ar = new Scanner(System.in);
+        System.out.println("====== Data diri =======");
+        
+        System.out.print("Nama : ");
+        nama = ar.nextLine();
+        System.out.print("Alamat : ");
+        alamat = ar.nextLine();
+        System.out.print("Umur : ");
+        umur = ar.nextInt();
+       
+        
+       
+        
+
+    }
+    
+}
