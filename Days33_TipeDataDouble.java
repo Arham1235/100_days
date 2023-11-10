3df4c2e13e4d6b405d468075325082d4655ee72c
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-10 22:28:36 +0800

    Add files via upload
    operator aritmatika menggunakan tipe data double

---
+++ Days33_TipeDataDouble.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days33_TipeDataDouble {
+    public static void main(String[] args) {
+        double a,b,c,d;
+        a = 1.2;
+        b = 150.23;
+        c = 123.5;
+        d = 167.0;
+       double ab = a+b;
+       double bc = b-c;
+       double cd = c*d;
+       double ad = a/d;
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        System.out.println(" ");
+        System.out.println("a + b = "+ab);
+        System.out.println("b - c = "+bc);
+        System.out.println("c * d = "+cd);
+        System.out.println("a / d ="+ad);
+        
+    }
+}
