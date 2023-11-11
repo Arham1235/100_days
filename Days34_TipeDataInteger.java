4c60bc9f06dc3f4575a810b0dc6dbec110b422e7
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-11 22:17:49 +0800

    Add files via upload
    Operator Aritmatika menggunakan tipe data Integer

---
+++ Days34_TipeDataInteger.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days34_TipeDataInteger {
+    public static void main(String[] args) {
+     
+    int a,b,c,d;
+    a = 200 ;
+    b = 20;
+    c = 100;
+    d = 10;
+    int ab = a+b;
+    int bc = b-c;
+    int cd = c*d;
+    int ad = a/d;
+    
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        System.out.println(" ");
+        System.out.println("a + b = "+ab);
+        System.out.println("b - c = "+bc);
+        System.out.println("c * d = "+cd);
+        System.out.println("a / d = "+ad);
+        
+        System.out.println("Nilai max = " + Integer.MAX_VALUE);
+        System.out.println("Nilai min = " + Integer.MIN_VALUE);
+    } 
+}
