ab54fa97dcfc7e907451a783fa0b3e299ce79d07
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-12 21:26:07 +0800

    Add files via upload
    tipe data float menggunkan operator aritmatika

---
+++ Days35_TipeData_Float.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days35_TipeData_Float {
+    public static void main(String[] args) {
+         byte a,b,c,d;
+    a = 120 ;
+    b = 2;
+    c = 3;
+    d = 10;
+    float  ab = a+b;
+    float  bc = b-c;
+    float  cd = c*d;
+    float  ad = a/d;
+    
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("c = "+c);
+        System.out.println("d = "+d);
+        System.out.println(" ");
+        System.out.println("a + b = "+ab);
+        System.out.println("b - c = "+bc);
+        System.out.println("c * d = "+cd);
+        System.out.println("a / d = "+ad);
+        
+        System.out.println("Nilai max = " + Float.MAX_VALUE);
+        System.out.println("Nilai min = " + Float.MIN_VALUE);
+    }
+    
+}
