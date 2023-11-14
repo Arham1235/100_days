7acad894be2f4565642c3f1b5e485776756a9a3c
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-14 20:23:28 +0800

    Add files via upload
    mangambil inputan dari sacanner menggunakan tipedata Long

---
+++ Days37_Scanner_TipeData_Long.java
@@ -0,0 +1,16 @@
+package ISC_Project;
+
+import java.util.Scanner;
+
+
+public class Days37_Scanner_TipeData_Long {
+    public static void main(String[] args) {
+        long nilai;
+        Scanner lon = new Scanner (System.in);
+         System.out.print("masukkan nilai positif  Long : ");
+         nilai = lon.nextLong();
+         System.out.print("Masukkan nilai negatif long : ");
+         nilai = lon.nextLong();
+        
+    }
+}
