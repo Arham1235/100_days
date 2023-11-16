988d9670ee91642a7c012a622c456ad5b35fad8e
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-16 18:01:37 +0800

    Add files via upload
    hari ini saya membuat project menggunkan  scanner dengan kombinasi if else

---
+++ Days39_percabangan_If_else.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arham
+ */
+public class Days39_percabangan_If_else {
+    public static void main(String[] args) {
+        int bebas;
+        Scanner ar = new Scanner (System.in);
+         System.out.print("masukkan umur :");
+         bebas = ar.nextInt();
+         if (bebas >20){
+             System.out.println("Anda sudah dewasa ");
+    
+         }else {
+             System.out.println("Anda masi remaja");
+         }
+        System.out.println("  Terimakasi ");
+    }
+    
+}
