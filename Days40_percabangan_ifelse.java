1a3c78b37f33be24a75b39727131a7ae990de6d0
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-17 15:21:43 +0800

    Add files via upload
    percabangan if else

---
+++ Days40_percabangan_ifelse.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days40_percabangan_ifelse {
+    public static void main(String[] args) {
+         int ar = 12;
+    if(ar == 10){
+            System.out.println("nilai ini sama dengan 11 ");
+    }else if (ar >15){
+        System.out.println("milai ini di atas 15 ");
+    }else{
+        System.out.println("nilai ini berada di bawah 15 dan di atas 10 ");
+    }
+   
+}
+    
+}
