1b58d4947e0292c42fcd87a99391a19efe94cbf4
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-20 21:26:41 +0800

    Add files via upload
    menambah koma di belakang angka

---
+++ Days43_perulangan_menambah_koma.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days43_perulangan_menambah_koma {
+    public static void main(String[] args) {
+        for (int i = 3; i < 10; i++) {
+            System.out.print(i);
+            if (i<9){
+                System.out.print(", ");
+            }
+              
+        }
+        System.out.println("");
+    }
+}
