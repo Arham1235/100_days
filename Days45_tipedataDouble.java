e46db9dd72b9186ea129f1313baa3995aa436e3e
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-22 21:08:06 +0800

    Add files via upload
    menjumlahkan nilai a dan b menggunakan tipe data double

---
+++ Days45_tipedataDouble.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days45_tipedataDouble {
+    public static void main(String[] args) {
+     double a,b;
+        a = 1.2;
+        b = 150.23;
+       
+       double ab = a+b;
+      
+       
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        System.out.println("a + b : "+ab);
+        
+    }
+    
+}
