af123f0da1cc2b15b4ed25de4ed4792fd5364dd9
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-23 20:53:57 +0800

    Add files via upload
    perulangan menggunakan tipe data integer, mencetak angka dimulai dari variabel A sampai angka variabel B

---
+++ Days46_Perulangan_TipeData_Integer.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days46_Perulangan_TipeData_Integer {
+    public static void main(String[] args) {
+         int a = 10;
+        int b = 19;
+        for (int i = a; i < b; i++) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
