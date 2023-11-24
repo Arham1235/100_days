240878594bed0f0abdc4bde0fb2f8a3a4177312b
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-24 20:18:06 +0800

    Add files via upload
    membuat perulangan menggunakan tipe data double

---
+++ Days47_Perulangan_TipeData_Double.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days47_Perulangan_TipeData_Double {
+    public static void main(String[] args) {
+        double a = 1.0;
+        double b = 10.0;
+        for (double i = a; i < b; i++) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
