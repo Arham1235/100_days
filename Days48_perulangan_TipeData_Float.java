0451f5db6cc92c9df9e733d1d96705143cdef590
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-25 20:52:10 +0800

    Add files via upload
    perulanggan menggunkan tipe data float

---
+++ Days48_perulangan_TipeData_Float.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days48_perulangan_TipeData_Float {
+    public static void main(String[] args) {
+        float a = 120 ;
+        float b = 130;
+        for (float i = a; i < b; i++) {
+            System.out.println(i);
+            
+        }
+    }
+    
+}
