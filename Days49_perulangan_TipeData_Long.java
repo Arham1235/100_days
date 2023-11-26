a0c4814ce0cde075695701deeeb7eb655ca86d18
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-26 20:01:11 +0800

    Add files via upload
    perulangan menggunakan tipe data long

---
+++ Days49_perulangan_TipeData_Long.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days49_perulangan_TipeData_Long {
+
+    public static void main(String[] args) {
+        long a = 12333323;
+        long b = 12333333;
+        for (long i = a; i < b; i++) {
+            System.out.println(i);
+
+        }
+
+    }
+}
