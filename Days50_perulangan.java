f6623b227491f15f569c0dbaf9fb2b3644b4c287
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-27 17:07:35 +0800

    Add files via upload
    membuat dua perulangan

---
+++ Days50_perulangan.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days50_perulangan {
+    public static void main(String[] args) {
+        for (int i = 6; i < 10; i++) {
+            System.out.println(i+2);
+            
+        }
+        for (int a = 2; a < 10; a++) {
+            System.out.println(a);
+            
+        }
+    }
+    
+}
