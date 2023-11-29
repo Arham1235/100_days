20a94e8f7f29ceca11b8ee27b7f328c91d8b130a
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-29 15:23:07 +0800

    Add files via upload
    mengambil data dari array

---
+++ Days52_MengambilDataDariArray.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days52_MengambilDataDariArray {
+
+    public static void main(String[] args) {
+        String[] nama = {"Arham", "muh Hikmal.R", "sarly", "Ahmad rahman"};
+        System.out.println("Nama Saya   : " + nama[0]);
+        System.out.println("Teman1      : " + nama[1]);
+        System.out.println("Teman2      : " + nama[2]);
+        System.out.println("Teman3      : " + nama[3]);
+    }
+
+}
