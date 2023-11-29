4933185c2bf8d23e95d4d7bab6c30381a8acbd50
Arham <147286846+Arham1235@users.noreply.github.com>
2023-11-29 15:24:23 +0800

    Add files via upload
    mengambil dan mencetak data secara berurutang menggunakan perulangan

---
+++ Days53_PerulanganArray.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days53_PerulanganArray {
+
+    public static void main(String[] args) {
+        String[] nama = {"Arham", "muh Hikmal.R", "sarly", "Ahmad rahman"};
+        for (int i = 0; i <nama.length; i++) {
+            System.out.println("index ke - "+i+" : "+nama[i]);
+            
+        }
+    }
+
+}
