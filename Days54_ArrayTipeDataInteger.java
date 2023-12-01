5e0072feee527d2597e7bf3db5fc0e6949003464
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-01 20:11:41 +0800

    Add files via upload
    perulangan array menggunakan tipedata integer

---
+++ Days54_ArrayTipeDataInteger.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days54_ArrayTipeDataInteger {
+     public static void main(String[] args) {
+        int[] nilai = {50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100};
+        for (int i = 0; i < nilai.length; i++) {
+            System.out.println( "  nilai : " + nilai[i]);
+
+        }
+
+    }
+    
+}
