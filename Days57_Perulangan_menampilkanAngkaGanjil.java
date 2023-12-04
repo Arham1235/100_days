d5ae6138f6086a5f8f10c81d9849c0c3e52bc739
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-04 23:27:50 +0800

    Add files via upload
    melakukan perulangan dengan menampilkan bilangan ganjil

---
+++ Days57_Perulangan_menampilkanAngkaGanjil.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days57_Perulangan_menampilkanAngkaGanjil {
+    public static void main(String[] args) {
+        
+        for(int i = 1; i <= 20; i += 2){
+            System.out.print( i + " ");
+        }
+    
+    }
+    
+}
