1d8a95162b6cfad14d4288d030f22eb78a539a5a
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-05 21:51:30 +0800

    Days58
    perulangan do while yang dimana akan mencetak angka yang berada di dalam variabel (i) sampai dengan batasan while

---
+++ Days58_perulanganDoWhile.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days58_perulanganDoWhile {
+    public static void main(String[] args) {
+        int i = 2;
+
+        do {
+            System.out.println("perulangan ke-" + i);
+            i++;
+        } while ( i <= 12);
+    }
+    
+    
+}
