b5525259a7bdb154b2197568b9166d80696671d0
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-06 21:37:32 +0800

    Days59
    menampilkan angka genap menggunakan perulangan

---
+++ Days59_Perulangan_MenampilkanAngkaGenap.java
@@ -0,0 +1,15 @@
+package ISC_Project;
+
+
+public class Days59_Perulangan_MenampilkanAngkaGenap {
+    public static void main(String[] args) {
+    
+        for (int i = 1; i < 50; i+=2) {
+            System.out.println(i);
+            
+        }
+
+        
+    }
+    
+}
