e74bdb7ff88544f336bd467b1f461f9afc49e745
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-07 20:18:51 +0800

    Add files via upload
    menampilkan angka dalam array menggunakan perulangan  For each

---
+++ Days60_perulanganForeach.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days60_perulanganForeach {
+
+    public static void main(String[] args) {
+
+        int angka[] = {0, 1, 2, 3, 4};
+
+        for (int x : angka) {
+            System.out.print(x + " ");
+        }
+
+    }
+
+}
