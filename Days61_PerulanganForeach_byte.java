ef67ee8187d663d7aa7c727b6b9b7e0d157d0af8
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-08 21:54:38 +0800

    Add files via upload
    perulangan For each menggunakan tipe data Byte

---
+++ Days61_PerulanganForeach_byte.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days61_PerulanganForeach_byte {
+
+    public static void main(String[] args) {
+
+        byte angka[] = {30, 61, 42, 13, 44};
+
+        for (byte x : angka) {
+            System.out.print(x + " ");
+
+        }
+        System.out.println("");
+    }
+
+}
