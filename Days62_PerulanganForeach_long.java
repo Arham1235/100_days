253d8fb1a767e4ae19160ed43b0268e6149a8b08
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-09 20:21:13 +0800

    Days62
    perulangan for each menggunakan tipe data long

---
+++ Days62_PerulanganForeach_long.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days62_PerulanganForeach_long {
+    public static void main(String[] args) {
+         long angka[] = {332110, 62331, 4242, 135533, 44434};
+
+        for (long x : angka) {
+            System.out.print(x + " ");
+
+        }
+        System.out.println("");
+    }
+}
