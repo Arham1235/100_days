d5a5094e35bbde1fd98d81f5fc59082217082c10
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-10 10:40:45 +0800

    Days63
    perulangan for each menggunakan tipe data double

---
+++ Days63_PerulanganForeach_Double.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days63_PerulanganForeach_Double {
+    public static void main(String[] args) {
+        double angka[] = {33.2110, 62.331, 4.242, 1.35533, 444.34};
+
+        for (double x : angka) {
+            System.out.print(x + " ");
+
+        }
+        System.out.println("");
+    }
+}
