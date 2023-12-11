2a702bbf40dd7c310de625e37183a6792ae9a456
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-11 22:42:56 +0800

    Days64
    perulangan for each menggunakan tipe data  float

---
+++ Days64_PerulanganForeach_float.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days64_PerulanganForeach_float {
+    public static void main(String[] args) {
+        float angka[] = {32110, 62331, 4242, 135533, 44434};
+
+        for (float x : angka) {
+            System.out.print(x + " ");
+
+        }
+        System.out.println("");
+    
+    }
+    
+}
