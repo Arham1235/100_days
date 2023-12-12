3906492c05fbab21afd5a0b660913bd15fdc6d26
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-12 21:25:32 +0800

    Add files via upload
    perulangan for each menggunakan tipe data integer

---
+++ Days65_PerulanganForeach_integer.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days65_PerulanganForeach_integer {
+    
+    public static void main(String[] args) {
+        int angka[] = {0, 541, 265, 563, 465};
+
+        for (int x : angka) {
+            System.out.print(x + " ");
+        }
+    }
+    
+}
