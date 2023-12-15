17c73ac34e50204073d075bc582653689252f2ab
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-15 18:21:49 +0800

    Days68
    tipe Data boolean

---
+++ Days68_boolean.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days68_boolean {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a);
+        System.out.println(b);
+    }
+    
+}
