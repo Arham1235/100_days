6fc4df44067bf939b15fbad82b51c66d37435fb5
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-16 23:12:02 +0800

    days69
    melakukan operator aritmatika menggunakan tipe data integer

---
+++ Days69_operatorAritmatika.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days69_operatorAritmatika {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 4;
+        int c = a+b;
+        int d = b*a;
+        
+    }
+    
+}
