ca23ad1638409f3b107f0ba5f5345882059ee06f
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-17 21:58:51 +0800

    Days70
    melakukan operator aritmatika menggunakan Scanner

---
+++ Days70_operatorAritmatika_Scanner.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arham
+ */
+public class Days70_operatorAritmatika_Scanner {
+     public static void main(String[] args) {
+        int a,b;
+        Scanner ar = new Scanner (System.in);
+        System.out.print("masukkan nilai A : ");
+        a=ar.nextInt();
+         System.out.print("masukkan nilai B : ");
+         b=ar.nextInt();
+         System.out.println("Hasil : "+(a+b));
+    }
+    
+    
+}
