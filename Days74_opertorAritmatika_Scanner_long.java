3c964203f3c4d0e7a6e869a4dd93760292e25db0
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-21 22:16:50 +0800

    Days74
    mengambil inputan dari scanner dengan tipe data long

---
+++ Days74_opertorAritmatika_Scanner_long.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arham
+ */
+public class Days74_opertorAritmatika_Scanner_long {
+
+    public static void main(String[] args) {
+       long a, b;
+        Scanner ar = new Scanner(System.in);
+        System.out.print("masukkan nilai A : ");
+        a = ar.nextLong();
+        System.out.print("masukkan nilai B : ");
+        b = ar.nextLong();
+        System.out.println("Hasil : " + (a + b));
+
+    }
+
+}
