1f0c569c3abe9e4a6455e9dd9d23d1b3b5d79490
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-25 21:47:11 +0800

    Days78
    tipeData char

---
+++ Days78_TipeDada_char.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+/**
+ *
+ * @author arham
+ */
+public class Days78_TipeDada_char {
+
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = 'r';
+        char c = 'h';
+        char d = 'a';
+        char e = 'm';
+
+        System.out.println(a);  
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+        System.out.print(d);
+        System.out.println(e);
+    }
+
+}
