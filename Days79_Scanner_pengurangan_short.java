c5edb7bbfca21d4fd38772a6b20af5873482f4f0
Arham <147286846+Arham1235@users.noreply.github.com>
2023-12-27 01:01:13 +0800

    Days79
    melakukan pengurangan menggunakan Scanner dengan tipe data Short

---
+++ Days79_Scanner_pengurangan_short.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arham
+ */
+public class Days79_Scanner_pengurangan_short {
+    public static void main(String[] args) {
+        short a,b;
+        Scanner ar = new Scanner(System.in);
+        System.out.print("masukkan nilai A : ");
+        a = ar.nextShort();
+        System.out.print("masukkan nilai B : ");
+        b = ar.nextShort();
+        System.out.println("hasil = "+(a-b));
+    }
+    
+}
