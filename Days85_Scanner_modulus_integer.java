e89f054d59a78321bfba5cc3ca06be74208fe0f9
Arham <147286846+Arham1235@users.noreply.github.com>
2024-01-01 20:28:41 +0800

    Days85
    melakukan modulus menggunakan Scanner dengan `tipe data integer

---
+++ Days85_Scanner_modulus_integer.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ISC_Project;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author arham
+ */
+public class Days85_Scanner_modulus_integer {
+
+    public static void main(String[] args) {
+        int a, b;
+        Scanner ar = new Scanner(System.in);
+        System.out.print("masukkan nilai A : ");
+        a = ar.nextShort();
+        System.out.print("masukkan nilai B : ");
+        b = ar.nextShort();
+        System.out.println("hasil = " + (a % b));
+    }
+
+}
